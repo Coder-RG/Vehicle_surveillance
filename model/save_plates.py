b334d6fc8cba5cf6df9c6b573324560a72054fc5
Coder-RG <rgoel1999@gmail.com>
2021-07-01 11:52:14 +0530

    Added video file and minor change to save_plates.py

--- model/save_plates.py
+++ model/save_plates.py
@@ -7,15 +7,9 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 # required by wpod-net
 import cv2
 import numpy as np
-# import matplotlib.pyplot as plt
-# import matplotlib.gridspec as gridspec
 from local_utils import detect_lp
 from os.path import splitext,basename
 from tensorflow.keras.models import model_from_json
-# from tensorflow.keras.preprocessing.image import load_img, img_to_array
-# from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
-# from sklearn.preprocessing import LabelEncoder
-# import glob
 from imageio import imwrite
 import logging
 logging.getLogger().setLevel(logging.ERROR)
